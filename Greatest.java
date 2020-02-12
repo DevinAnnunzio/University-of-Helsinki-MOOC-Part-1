f7a9641e4998a8ab4eab6c236c937f472319f265
Devin Annunzio <49703175+DevinAnnunzio@users.noreply.github.com>
2020-02-12 00:57:50 -0500

    Exercise 45

---
+++ Greatest.java
@@ -0,0 +1,22 @@
+
+public class Greatest {
+
+    public static int greatest(int number1, int number2, int number3) {
+
+        int result = Math.max(number1, number2);
+        
+        if(result > number3){
+            return result;
+        }
+        
+        else {
+            return number3;
+        }
+     
+    }
+
+    public static void main(String[] args) {
+        int result = greatest(2, 7, 3);
+        System.out.println("Greatest: " + result);
+    }
+}
