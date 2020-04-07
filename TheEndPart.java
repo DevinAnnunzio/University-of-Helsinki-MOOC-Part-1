d05744d5453c5e46bd126743e9dba31a4fff44a0
Devin Annunzio <49703175+DevinAnnunzio@users.noreply.github.com>
2020-04-07 16:20:03 +0900

    Exercise 54

---
+++ TheEndPart.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TheEndPart {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+                
+        System.out.println("Type a word: ");
+        
+        String word = reader.next();
+        
+        System.out.println("Length of the end part");
+        
+        int length = reader.nextInt();
+        
+        
+        System.out.println("Result: " + word.substring(word.length()-length));
+        
+    }
+}
