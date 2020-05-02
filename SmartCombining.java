695f5421d271492e0bf5dd4d399f335b57e5ed14
Devin Annunzio <49703175+DevinAnnunzio@users.noreply.github.com>
2020-05-02 15:21:13 +0900

    Exercise 71

---
+++ SmartCombining.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SmartCombining {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+        Collections.addAll(list1, 4, 3);
+
+        Collections.addAll(list2, 5, 10, 4, 3, 7);
+
+        // remove comment when method ready
+        smartCombine(list1, list2);
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+    
+    public static void smartCombine(ArrayList<Integer>list1, ArrayList<Integer> list2){
+       
+        
+        for(int numberToFind:list2){
+            //if list1 doesn't contain the number, add it
+            if(!list1.contains(numberToFind)){
+                list1.add(numberToFind);
+            }
+            
+            
+        }
+        
+        
+        
+    }
+
+}
